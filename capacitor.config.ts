b271bd7d7c68f7ccae843ee9dd32b96bb040ff0a
Leonel <leonel@shake.app>
2026-05-04 16:52:40 -0600

    Add PushNotifications plugin config to capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,9 @@ const config: CapacitorConfig = {
   appName: 'SHAKE',
   webDir: 'dist',
   plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
     CapacitorPurchases: {
       androidKey: 'YOUR_GOOGLE_PLAY_PUBLIC_KEY', // Add later
       iosKey: 'YOUR_APPLE_APP_STORE_KEY', // Add later
